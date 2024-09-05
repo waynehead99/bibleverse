bf024737024cd181b96875747ffe366d6909b3de
waynehead99 <waynehead99@hotmail.com>
2024-09-04 23:14:23 -0600

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,9 +1,9 @@
 <?php
 // Database credentials
-$host = getenv('MYSQL_HOST');
-$db   = getenv('MYSQL_DATABASE');
-$user = getenv('MYSQL_USER');
-$pass = getenv('MYSQL_PASSWORD');
+$host = '10.0.248.15;
+$db   = 'bible_verses';
+$user = 'sqlsev';
+$pass = 'Gamma99Quest';
 
 
 // Create connection
